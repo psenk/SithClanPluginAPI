9acad10a625f0e9961c823e53326af0719460a65
Tyler <psenk168@gmail.com>
2026-03-04 20:58:45 -0600

    fetch function updated to retrieve test schedule updated to switch case statement formatted schedule into json

--- src/index.js
+++ src/index.js
@@ -9,16 +9,32 @@
  */
 
 export default {
+	/**
+	 * Handles incoming HTTP requests.
+	 *
+	 * @param   {Request} request incoming HTTP request
+	 * @param   {Env}     env     wrangler worker bindings and environment variables
+	 * @returns {Response}        HTTP response
+	 */
 	async fetch(request, env) {
 		const { pathname } = new URL(request.url);
 
-		if (pathname === 'api/beverages') {
-			const { results } = await env.sithclanplugindatabase
-				.prepare('SELECT * FROM Customers WHERE CompanyName = ?')
-				.bind('Bs Beverages')
-				.run();
-			return Response.json(results);
+		switch (pathname) {
+			case '/api/testschedule':
+				const { results } = await env.sithclanplugindatabase.prepare('SELECT * FROM TestEventSchedule WHERE EventId = 1;').run();
+
+				const parsedSchedule = results.map((row) => ({
+					...row,
+					EventSchedule: JSON.parse(row.EventSchedule),
+				}));
+				return Response.json(parsedSchedule);
+
+			case '/api/eventschedule':
+				// TODO: implement functionality
+				return new Response('TBD');
+
+			default:
+				return new Response('Not found', { status: 404 });
 		}
-		return new Response('Call /api/beverages to see everyone who works at Bs Beverages');
 	},
 };
