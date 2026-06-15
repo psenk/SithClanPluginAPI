e53a571f42e2fdc39d0c487108fa94f4407f0cf6
psenk <psenk168@gmail.com>
2026-06-14 19:07:10 -0500

    fix endpoint bug

--- src/index.js
+++ src/index.js
@@ -145,6 +145,19 @@ router.post('/api/memberroster', async (request, env) => {
 // Member About Me
 // --------------------
 
+// GET - fetch all members about me
+router.get('/api/aboutme', async (request, env) => {
+	const { results } = await env.sithclanplugindatabase.prepare('SELECT MemberName, AboutMe FROM MemberAboutMe;').run();
+
+	// build map of member name, about me
+	const aboutMeMap = {};
+	for (const row of results) {
+		aboutMeMap[row.MemberName.toLowerCase()] = row.AboutMe ?? '';
+	}
+
+	return Response.json(aboutMeMap);
+});
+
 // GET - fetch specific member about me
 router.get('/api/aboutme/:memberName', async (request, env) => {
 	const { memberName } = request.params;
@@ -161,19 +174,6 @@ router.get('/api/aboutme/:memberName', async (request, env) => {
 	}
 
 	return Response.json({ aboutMe: result.AboutMe ?? '' });
-});
-
-// GET - fetch all members about me
-router.get('/api/aboutme', async (request, env) => {
-	const { results } = await env.sithclanplugindatabase.prepare('SELECT MemberName, AboutMe FROM MemberAboutMe;').run();
-
-	// build map of member name, about me
-	const aboutMeMap = {};
-	for (const row of results) {
-		aboutMeMap[row.MemberName.toLowerCase()] = row.AboutMe ?? '';
-	}
-
-	return Response.json(aboutMeMap);
 });
 
 // PUT - submit or update member about me
